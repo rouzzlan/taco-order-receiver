46d4fc2d54aced78e479e7d06d529468a931a804
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-05-01 16:04:43 +0200

    initial commit

---
+++ src/main/java/com/falcontech/orderreceiver/OrderReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.falcontech.orderreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderReceiverApplication.class, args);
+	}
+
+}
